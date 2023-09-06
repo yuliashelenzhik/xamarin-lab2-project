18dcee71bcb42dcaa21dd58e5725169f91dab97b
yuliayuste <yuliashelenzhik@qq.com>
2023-09-06 19:34:03 +0200

    Navigation from Menu added

--- lab2-project/lab2-project/MenuView.xaml.cs
+++ lab2-project/lab2-project/MenuView.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using lab2project;
 using Xamarin.Forms;
 
 namespace lab2project
@@ -11,6 +11,18 @@ namespace lab2project
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private async void Goto_Rating(object sender, EventArgs e)
+        {
+            // Navigate to the RatingPage when the "Rating" button is clicked
+            await Navigation.PushAsync(new ImageRating());
+        }
+
+        private async void Goto_Admin(object sender, EventArgs e)
+        {
+            // Navigate to the RatingPage when the "Rating" button is clicked
+            await Navigation.PopAsync();
+        }
+    }
 }
 
